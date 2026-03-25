f5fe3f4a075d6f80b5a4b17f51ea2372684e0efe
Rafael Kitover <rkitover@gmail.com>
2026-03-25 21:00:17 +0000

    wxwidgets: update master hash + bump ver
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- wxwidgets/portfile.cmake
+++ wxwidgets/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO wxWidgets/wxWidgets
     REF master
-    SHA512 c9ac27ac87fe1f09b39424673fabd44a5ff8cb09c2ed0ced854e2994e3fce19d6d5f60b4f9fbf1f55478e19d0d498f57a49018cc885bad9c332a3f9c7b68935a
+    SHA512 570bb07fd83d7bd1d6958e833f663e4061082e41a2d9f4e94e3d99dfc22a8afc90ba9b28397501aeab68c4a4ab59fd126413bb5cdf9af2591e947799d3e5748f
     PATCHES
         install-layout.patch
         relocatable-wx-config.patch
@@ -22,7 +22,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH lexilla_SOURCE_PATH
     REPO wxWidgets/lexilla
     REF "0dbce0b418b8b3d2ef30304d0bf53ff58c07ed84"
-    SHA512 61f7b0217f4518121ecad32f015b53600e565bdd499d4020468cf6c8a533d516c6881115aa5e640afca5ea428535f47680cde426fa3dbabe9e4423b4526853fd
+    SHA512 570bb07fd83d7bd1d6958e833f663e4061082e41a2d9f4e94e3d99dfc22a8afc90ba9b28397501aeab68c4a4ab59fd126413bb5cdf9af2591e947799d3e5748f
     HEAD_REF wx
 )
 file(COPY "${lexilla_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/src/stc/lexilla")
@@ -30,7 +30,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH scintilla_SOURCE_PATH
     REPO wxWidgets/scintilla
     REF "0b90f31ced23241054e8088abb50babe9a44ae67"
-    SHA512 db1f3007f4bd8860fad0817b6cf87980a4b713777025128cf5caea8d6d17b6fafe23fd22ff6886d7d5a420f241d85b7502b85d7e52b4ddb0774edc4b0a0203e7
+    SHA512 570bb07fd83d7bd1d6958e833f663e4061082e41a2d9f4e94e3d99dfc22a8afc90ba9b28397501aeab68c4a4ab59fd126413bb5cdf9af2591e947799d3e5748f
     HEAD_REF wx
 )
 file(COPY "${scintilla_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/src/stc/scintilla")
