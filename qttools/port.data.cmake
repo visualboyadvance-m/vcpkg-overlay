ee44f96a385fd0b89aad6410940e047c72ac4071
Andy Vandijck <andyvand@gmail.com>
2026-09-04 09:50:59 +0200

    qttools 6.11.2

---
+++ qttools/port.data.cmake
@@ -0,0 +1,3 @@
+set(qttools_HASH "2c420cbafbcfa300168e1c46d0e8a71e15671b065e6a2190eebcb4f1456cc95175c0e8ce0831ab9e4cca7263ba18c0320f4a1c677c4aa2f796c19afe8a76f482")
+set(qttools_URL "https://download.qt.io/archive/qt/6.11/6.11.2/submodules/qttools-everywhere-src-6.11.2.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.11/6.11.2/submodules/qttools-everywhere-src-6.11.2.tar.xz")
+set(qttools_FILENAME "qttools-everywhere-src-6.11.2.tar.xz")
